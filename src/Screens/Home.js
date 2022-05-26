a9100e1a4c1e65c9aec99d73b2e371b89b696c28
emreuzun1 <uzun.emre1@outlook.com>
2022-05-26 16:34:44 +0300

    second

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -4,18 +4,34 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { AiFillLinkedin, AiFillInstagram, AiFillGithub } from "react-icons/ai";
+import Navbar from "../components/Navbar";
 
 gsap.registerPlugin(ScrollToPlugin);
 gsap.registerPlugin(ScrollTrigger);
 
 export const Home = () => {
   useEffect(() => {
-    let tl = gsap.timeline({ delay: 0.1 });
+    let tl = gsap.timeline({
+      delay: 0.1,
+    });
+
+    gsap.to(".first-section", {
+      scrollTrigger: {
+        trigger: ".section-cap",
+        end: "+=300",
+        scrub: 1,
+      },
+      y: -20,
+      opacity: 0,
+    });
+
+    tl.fromTo(".caption-title", { y: -20, opacity: 0 }, { y: 0, opacity: 1 });
 
     tl.fromTo(
       ".h1",
       {
         x: -20,
+        opacity: 0,
       },
       { duration: 0.2, x: 0, ease: "power4.inOut", opacity: 1, stagger: 0.2 }
     );
@@ -47,6 +63,36 @@ export const Home = () => {
     );
     tl.fromTo(".contact-bar", { width: 0 }, { width: "50%" });
     tl.fromTo(".contact-icon", { opacity: 0 }, { opacity: 1 });
+
+    let secondtl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".second-cap",
+        end: "+=500",
+        scrub: 1,
+      },
+    });
+    secondtl.fromTo(
+      ".second-title",
+      { y: -20, opacity: 0 },
+      { y: 0, opacity: 1 }
+    );
+    secondtl.fromTo(
+      ".second-p",
+      {
+        x: -20,
+        opacity: 0,
+      },
+      { duration: 0.2, x: 0, ease: "power4.inOut", opacity: 1, stagger: 0.2 }
+    );
+    secondtl.fromTo(
+      ".about-text",
+      { x: -20, opacity: 0 },
+      {
+        opacity: 1,
+        x: 0,
+        ease: "power4.inOut",
+      }
+    );
   }, []);
 
   return (
@@ -54,20 +100,24 @@ export const Home = () => {
       <div className="contact">
         <div className="contact-item">
           <div className="contact-bar" />
-          <AiFillLinkedin size={24} color="black" className="contact-icon" />
+          <AiFillLinkedin size={24} className="contact-icon" />
         </div>
         <div className="contact-item">
           <div className="contact-bar" />
-          <AiFillInstagram size={24} color="black" className="contact-icon" />
+          <AiFillInstagram size={24} className="contact-icon" />
         </div>
         <div className="contact-item">
           <div className="contact-bar" />
-          <AiFillGithub size={24} color="black" className="contact-icon" />
+          <AiFillGithub size={24} className="contact-icon" />
         </div>
       </div>
+      <Navbar />
       <div className="sections"></div>
       <div className="first-section">
         <div className="h1-container">
+          <div className="caption">
+            <div className="caption-title">01. Hello World!</div>
+          </div>
           <p className="h1 h1-open">{"<h1>"}</p>
           <div className="hi-container">
             <p className="hi">
@@ -83,7 +133,43 @@ export const Home = () => {
         </div>
       </div>
       <div className="second-section">
-        
+        <div className="section-caption">
+          <div className="caption second-cap">
+            <div className="caption-title second-title">02. About Me</div>
+            <div className="p-title second-p">{"<p>"}</div>
+            <div className="about-text">
+              I was born in İzmir in 1999. I've started to write code when I was
+              14. At my 16, I built a trojan virus. I graduated from Yaşar
+              University in 2022. I've done my internship at Jotform. Since
+              then, I am working at Cevher Digital Solutions as a Software
+              Engineer.
+            </div>
+            <div className="p-title second-p">{"</p>"}</div>
+            <div className="about-image-list">
+              <div className="about-image-item">
+                <img
+                  src={require("../assets/images/img1.png")}
+                  alt="1"
+                  className="about-image c-ratio"
+                />
+              </div>
+              <div className="about-image-item">
+                <img
+                  src={require("../assets/images/img2.png")}
+                  alt="1"
+                  className="about-image c-ratio"
+                />
+              </div>
+              <div className="about-image-item">
+                <img
+                  src={require("../assets/images/img1.png")}
+                  alt="1"
+                  className="about-image c-ratio"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import "./Navbar.styles.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="section-item">Home</div>
+      <div className="section-item">About</div>
+    </div>
+  );
+};
+
+export default Navbar;
